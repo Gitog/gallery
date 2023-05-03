ed095ca7c179003f5ae571d7598179e50b4bb9e8
James <jamesnjorio@gmail.com>
2023-05-03 10:52:06 +0300

    Created navbar reusable Components

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,68 @@
+function Navigation(){
+    return(
+        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="#">
+                Home
+              </a>
+            </li>
+          </ul>
+    )
+}
+
+
+function SearchForm(){
+    return(
+        <form className="d-flex" role="search">
+        <input
+          className="form-control me-2"
+          type="search"
+          placeholder="Search"
+          aria-label="Search"
+        />
+        <button className="btn btn-outline-success" type="submit">
+          Search
+        </button>
+        </form>
+    )
+}
+
+
+function Dropdown(){
+    return(
+        <ul className="navbar-nav mb-2 mb-lg-0">
+        {" "}
+    <li className="nav-item dropdown">
+      <a
+        className="nav-link dropdown-toggle"
+        href="#"
+        role="button"
+        data-bs-toggle="dropdown"
+        aria-expanded="false"
+      >
+        Login
+      </a>
+      <ul className="dropdown-menu">
+        <li>
+          <a className="dropdown-item" href="#">
+            Profile
+          </a>
+        </li>
+      </ul>
+    </li>
+    </ul>
+    )
+ 
+}
+
+
+
 function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light mb-5">
       <div className="container-fluid">
         <a className="navbar-brand" href="#">
-          Navbar
+          Scenes
         </a>
         <button
           className="navbar-toggler"
@@ -17,44 +76,11 @@ function Navbar() {
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">
-                Home
-              </a>
-            </li>
-          </ul>
-          <form className="d-flex" role="search">
-            <input
-              className="form-control me-2"
-              type="search"
-              placeholder="Search"
-              aria-label="Search"
-            />
-            <button className="btn btn-outline-success" type="submit">
-              Search
-            </button>
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item dropdown">
-              <a
-                className="nav-link dropdown-toggle"
-                href="#"
-                role="button"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-              >
-                Login
-              </a>
-              <ul className="dropdown-menu">
-                <li>
-                  <a className="dropdown-item" href="#">
-                    SignUp
-                  </a>
-                </li>
-              </ul>
-            </li>
-            </ul>
-          </form>
+            <Navigation />
+            <SearchForm />
+            <Dropdown />
+        
+           
         </div>
       </div>
     </nav>
