45b8816311726b8815308769067b6a31016bc612
James <jamesnjorio@gmail.com>
2023-05-03 09:44:00 +0300

    Looped image component to 9

--- src/App.js
+++ src/App.js
@@ -85,16 +85,20 @@ function App() {
         <h1>Gallery</h1>
 
         <div className="row">
-
-        <div class="card" style={{width: "18rem"}}>
-  <img src="https://via.placeholder.com/200" class="card-img-top" alt="image"/>
-  <div class="card-body">
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-  </div>
-</div>
-
+          {Array.apply(null, { length: 9 }).map(() => {
+            return (
+              <div className="col mb-5">
+              <div class="card" style={{ width: "18rem" }}>
+                <img
+                  src="https://via.placeholder.com/200"
+                  class="card-img-top"
+                  alt="image"
+                />
+              </div>
+              </div>
+            );
+          })}
         </div>
-
       </div>
     </>
   );
