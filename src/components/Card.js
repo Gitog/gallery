d6b0b453a20c4531d67b219bfeabef8f47d90792
James <jamesnjorio@gmail.com>
2023-05-03 10:13:50 +0300

    Created and Added Components

---
+++ src/components/Card.js
@@ -0,0 +1,15 @@
+function Card() {
+  return(
+    <div className="col mb-5">
+    <div className="Card" style={{ width: "18rem" }}>
+      <img
+        src="https://via.placeholder.com/200"
+        className="card-img-top"
+        alt="image"
+      />
+    </div>
+    </div>
+  );
+}
+
+export default Card;
